f03e98d3c775ba6e128f30b71f27dbd094caee78
MilenaCastilloC <sm.castillo@uniandes.edu.co>
2023-01-28 21:16:31 -0500

    Creacion Aplicación

--- Dermo-Web-App/src/app/app.component.spec.ts
+++ Dermo-Web-App/src/app/app.component.spec.ts
@@ -1,13 +1,9 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
       declarations: [
         AppComponent
       ],
@@ -20,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'dermoAppWeb'`, () => {
+  it(`should have as title 'Dermo-Web-App'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('dermoAppWeb');
+    expect(app.title).toEqual('Dermo-Web-App');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('dermoAppWeb app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Dermo-Web-App app is running!');
   });
 });
