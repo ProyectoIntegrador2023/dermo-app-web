8ecef50c6fd2706cfde86f48ec325987dfb19c13
MilenaCastilloC <sm.castillo@uniandes.edu.co>
2023-01-29 19:28:40 -0500

    Update karma.conf.js

--- Dermo-Web-App/karma.conf.js
+++ Dermo-Web-App/karma.conf.js
@@ -27,11 +27,11 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessCI'],
     customLaunchers: {
       ChromeHeadlessCI: {
         base: 'ChromeHeadless',
-        flags: ['--no-sandbox', '--disable-gpu']
+        flags: ['--no-sandbox']
       }
     },
     singleRun: false
