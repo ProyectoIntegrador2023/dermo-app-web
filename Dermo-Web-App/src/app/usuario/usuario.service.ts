87d45d7eef84023bb96aaed1b9f1d47f8b741869
MilenaCastilloC <sm.castillo@uniandes.edu.co>
2023-02-04 18:57:34 -0500

    Configuracion clase Usuario

--- Dermo-Web-App/src/app/usuario/usuario.service.ts
+++ Dermo-Web-App/src/app/usuario/usuario.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioService {
 
-constructor() { }
+    private backUrl: string = "http://localhost:5000"
+
+    constructor(private htt: HttpClient) { }
 
 }
