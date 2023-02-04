706a1c7662768b6bc062b9b57a2359e1929a4f2e
MilenaCastilloC <sm.castillo@uniandes.edu.co>
2023-02-04 18:42:26 -0500

    Creacion componente usuario-signup

---
+++ Dermo-Web-App/src/app/usuario/usuario-signup/usuario-signup.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-usuario-signup',
+  templateUrl: './usuario-signup.component.html',
+  styleUrls: ['./usuario-signup.component.css']
+})
+export class UsuarioSignupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
