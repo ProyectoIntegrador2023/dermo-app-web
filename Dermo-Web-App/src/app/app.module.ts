33aa97550d4479db94f2435f8b5c82e8c04eaf6a
MilenaCastilloC <sm.castillo@uniandes.edu.co>
2023-02-04 19:07:15 -0500

    Imports app.module.ts

--- Dermo-Web-App/src/app/app.module.ts
+++ Dermo-Web-App/src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { UsuarioModule } from './usuario/usuario.module';
+import { AppHeaderModule } from './app-header/app-header.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    UsuarioModule,
+    AppHeaderModule,
+    ToastrModule.forRoot({
+      timeOut: 7000,
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true,
+    }),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
