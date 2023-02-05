0aa1ad2eabe1e50522f5cbfdb4da1d91accc9d5f
MilenaCastilloC <sm.castillo@uniandes.edu.co>
2023-02-05 13:42:49 -0500

    Configuracion servicio login

--- Dermo-Web-App/src/app/usuario/usuario.module.ts
+++ Dermo-Web-App/src/app/usuario/usuario.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UsuarioSignupComponent } from './usuario-signup/usuario-signup.component';
+import { UsuarioLoginComponent } from './usuario-login/usuario-login.component';
 import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
-  declarations: [UsuarioSignupComponent],
+  declarations: [UsuarioLoginComponent, UsuarioSignupComponent],
   imports: [
     CommonModule, ReactiveFormsModule],
-  exports: [UsuarioSignupComponent]
+  exports: [UsuarioLoginComponent, UsuarioSignupComponent]
 })
 export class UsuarioModule { }
