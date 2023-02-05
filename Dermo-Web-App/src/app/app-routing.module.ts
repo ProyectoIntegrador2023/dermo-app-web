e9bf8e0be7c3b2770b9a5dbec4fc3d33b38363a0
MilenaCastilloC <sm.castillo@uniandes.edu.co>
2023-02-04 20:59:24 -0500

    Config app routing

--- Dermo-Web-App/src/app/app-routing.module.ts
+++ Dermo-Web-App/src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { UsuarioSignupComponent } from './usuario/usuario-signup/usuario-signup.component';
 
+const routes: Routes = [
+  {
+    path: 'signup',
+    component: UsuarioSignupComponent,
+    pathMatch: 'full'
+  }
+]
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- Dermo-Web-App/src/app/app.module.ts
+++ Dermo-Web-App/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { UsuarioModule } from './usuario/usuario.module';
 import { AppHeaderModule } from './app-header/app-header.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { AppHeaderModule } from './app-header/app-header.module';
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     HttpClientModule,
     UsuarioModule,
     AppHeaderModule,
