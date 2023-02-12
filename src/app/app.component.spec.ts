af5ce079d730cede74a435da7b5b4b0bafd837dc
MilenaCastilloC <sm.castillo@uniandes.edu.co>
2023-02-12 00:14:43 -0500

    Actualiza spect.ts app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,13 +23,13 @@ describe('AppComponent', () => {
   it(`should have as title 'Dermo-Web-App'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Dermo-Web-App');
+    expect(app.title).toEqual('Dermo App');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Dermo-Web-App');
+    expect(compiled.querySelector('.content h2')?.textContent).toContain('Dermo App');
   });
 });
