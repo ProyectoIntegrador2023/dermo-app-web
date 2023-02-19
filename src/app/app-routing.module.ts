1f608d152056e141bd90b71a80061b0bab932a50
MilenaCastilloC <sm.castillo@uniandes.edu.co>
2023-02-19 14:45:56 -0500

    Actualiza rutas imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { UsuarioLoginComponent } from './usuario/usuario-login/usuario-login.component';
-import { UsuarioSignupComponent } from './usuario/usuario-signup/usuario-signup.component';
+import { UsuarioLoginComponent } from './components/usuario/usuario-login/usuario-login.component';
+import { UsuarioSignupComponent } from './components/usuario/usuario-signup/usuario-signup.component';
 
 
 const routes: Routes = [
