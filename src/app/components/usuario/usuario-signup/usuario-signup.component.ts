a02840559e182121f493219ae4677a4714e910ca
MilenaCastilloC <sm.castillo@uniandes.edu.co>
2023-02-19 16:49:21 -0500

    configura evento close

--- src/app/components/usuario/usuario-signup/usuario-signup.component.ts
+++ src/app/components/usuario/usuario-signup/usuario-signup.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { UsuarioService } from '../usuario.service';
 import { JwtHelperService } from "@auth0/angular-jwt";
-import { Title } from '@angular/platform-browser';
+import { UserSignUpRq } from '../models/userSignUp.model';
 
 @Component({
   selector: 'app-usuario-signup',
@@ -14,43 +14,63 @@ import { Title } from '@angular/platform-browser';
 export class UsuarioSignupComponent implements OnInit {
 
   helper = new JwtHelperService();
-  usuarioForm: FormGroup;
+  signUpDto: UserSignUpRq;
+  viewPassword: boolean = false;
+
+  @Output() closeSignUp = new EventEmitter<boolean>();
 
   constructor(
     private usuarioService: UsuarioService,
     private formBuilder: FormBuilder,
     private router: Router,
-    private toastr: ToastrService,
-    private titulo:Title
-  ) {
-    titulo.setTitle('Registro')
-  }
+    private toastr: ToastrService
+  ) { }
+
+  signUpForm: FormGroup;
 
   ngOnInit() {
-    this.usuarioForm = this.formBuilder.group({
-      nombre: ["", [Validators.required, Validators.maxLength(50)]],
-      password: ["", [Validators.required, Validators.maxLength(50), Validators.minLength(4)]],
-      confirmPassword: ["", [Validators.required, Validators.maxLength(50), Validators.minLength(4)]]
-    })
+    this.signUpForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.maxLength(50)]],
+      password: ['', [Validators.required, Validators.maxLength(50), Validators.minLength(4)]],
+      confirmPassword: ['', [Validators.required, Validators.maxLength(50), Validators.minLength(4)]]
+    });
   }
 
-  registrarUsuario(){
-    this.usuarioService.userSignUp(this.usuarioForm.get('nombre')?.value, this.usuarioForm.get('password')?.value)
-    .subscribe(res => {
-      console.log(res)
-      this.router.navigate([`/signin`])
-      this.showSuccess()
-    },
-    error => {
-      this.showError(`Ha ocurrido un error: ${error.message}`)
-    })
+  error = false;
+
+  onSignUpUsuario() {
+    this.error = false
+    this.signUpDto = this.signUpForm.value;
+    this.usuarioService.userSignUp(this.signUpDto)
+      .subscribe(res => {
+        console.log(res)
+        const token = res.lastLoginAt;
+        sessionStorage.setItem('lastLoginAt', token);
+        this.router.navigate([`/signup`])
+        this.showSuccess()
+      },
+
+        error => {
+          this.showError(`Ha ocurrido un error: ${error.message}`);
+          this.error = true
+        })
   }
 
-  showError(error: string){
+  showError(error: string) {
     this.toastr.error(error, "Error")
   }
 
   showSuccess() {
     this.toastr.success(`Se ha registrado exitosamente`, "Registro exitoso");
+  }
+
+  changeType(id: string) {
+    const type = document.getElementById(id)?.getAttribute('type') === 'password' ? 'text' : 'password';
+    document.getElementById(id)?.setAttribute('type', type);
+    this.viewPassword = !this.viewPassword;
+  }
+
+  close() {
+    this.closeSignUp.emit(false);
   }
 }
