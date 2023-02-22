151aa3afede5e9e0eb652a5c6ff0e343e7b23ad7
MilenaCastilloC <sm.castillo@uniandes.edu.co>
2023-02-21 20:07:19 -0500

    Creacion componente header-privado

---
+++ src/app/components/header-privado/header-privado.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-privado',
+  templateUrl: './header-privado.component.html',
+  styleUrls: ['./header-privado.component.css']
+})
+export class HeaderPrivadoComponent {
+
+}
