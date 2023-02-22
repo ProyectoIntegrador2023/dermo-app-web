e1bc83e2066ec8787de36876f2094cdfc2d59165
MilenaCastilloC <sm.castillo@uniandes.edu.co>
2023-02-21 20:08:05 -0500

    Creación modulo crear-perfil

---
+++ src/app/components/crear-perfil/crear-perfil.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crear-perfil',
+  templateUrl: './crear-perfil.component.html',
+  styleUrls: ['./crear-perfil.component.css']
+})
+export class CrearPerfilComponent {
+
+}
