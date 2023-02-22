6ae8e0e574ea038a7535f1b286a84a4bae75ff80
MilenaCastilloC <sm.castillo@uniandes.edu.co>
2023-02-22 00:36:33 -0500

    actualiza routing home-in

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BienvenidoComponent } from './components/bienvenido/bienvenido.component';
+import { CrearPerfilComponent } from './components/crear-perfil/crear-perfil.component';
 import { HomeInComponent } from './components/home-in/home-in.component';
 import { HomeOutComponent } from './components/home-out/home-out.component';
 
@@ -12,10 +14,19 @@ const routes: Routes = [
   {
     path: 'home-in',
     component: HomeInComponent,
-    pathMatch: 'full'
+    children: [
+      {
+        path: '',
+        component: BienvenidoComponent
+      },
+      {
+        path: 'crear-perfil',
+        component: CrearPerfilComponent
+      }
+    ]
   },
-]
 
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
