540f5bcf9b9a80b3c6b8c198d4d5a378e02c3775
MilenaCastilloC <sm.castillo@uniandes.edu.co>
2023-02-23 00:08:27 -0500

    actualizacion spec home-in

--- src/app/components/home-in/home-in.component.spec.ts
+++ src/app/components/home-in/home-in.component.spec.ts
@@ -1,4 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HeaderPrivadoComponent } from '../header-privado/header-privado.component';
+import { HeaderComponent } from '../header/header.component';
+import { SideBarNavegacionComponent } from '../side-bar-navegacion/side-bar-navegacion.component';
 
 import { HomeInComponent } from './home-in.component';
 
@@ -8,9 +12,14 @@ describe('HomeInComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HomeInComponent ]
+      imports: [RouterTestingModule],
+      declarations: [
+        HomeInComponent,
+        SideBarNavegacionComponent,
+        HeaderComponent,
+        HeaderPrivadoComponent]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(HomeInComponent);
     component = fixture.componentInstance;
