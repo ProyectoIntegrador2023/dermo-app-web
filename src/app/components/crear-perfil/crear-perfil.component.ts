69c53567935817a3c257d8080a4d53542dd2efbf
MilenaCastilloC <sm.castillo@uniandes.edu.co>
2023-02-23 21:42:05 -0500

    creacion formulario crear-perfil

--- src/app/components/crear-perfil/crear-perfil.component.ts
+++ src/app/components/crear-perfil/crear-perfil.component.ts
@@ -12,15 +12,28 @@ export class CrearPerfilComponent implements OnInit {
   formCrearPerfil!: FormGroup;
 
   constructor(private formBuilder: FormBuilder) {
-    this.formCrearPerfil = this.formBuilder.group({
-      nombre: [{value: '', disabled: false}, [Validators.required, Validators.maxLength(10)]],
-      edad: [{value:'', disabled:false}, [Validators.required, Validators.maxLength(2)]],
-      pais: [{value: 1, disabled: false}, [Validators.required]],
-      ciudad: [{value: '', disabled: false}, [Validators.required, Validators.maxLength(10)]]
-    })
   }
   ngOnInit(): void {
+    this.formCrearPerfil = this.formBuilder.group({
+      nombre: [{ value: '', disabled: false }, [Validators.required, Validators.maxLength(10)]],
+      edad: [{ value: '', disabled: false }, [Validators.required, Validators.maxLength(2)]],
+      pais: [{ value: 1, disabled: false }, [Validators.required]],
+      ciudad: [{ value: '', disabled: false }, [Validators.required, Validators.maxLength(10)]]
+    })
+  }
 
+  guardar() {
+    console.log(this.formCrearPerfil);
+    console.log(this.formCrearPerfil.value);
+    console.log(this.formCrearPerfil.value.nombre);
+    console.log(this.formCrearPerfil.value.edad);
+    console.log(this.formCrearPerfil.value.pais);
+    console.log(this.formCrearPerfil.value.ciudad);
+  }
+
+  get formPerfil() {
+    return this.formCrearPerfil.controls;
   }
 
 }
+
