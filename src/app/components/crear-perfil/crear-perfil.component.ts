09aa885b28dede90e068ff9d41976b638b28d8fb
MilenaCastilloC <sm.castillo@uniandes.edu.co>
2023-02-23 20:09:24 -0500

    agrega constructor form

--- src/app/components/crear-perfil/crear-perfil.component.ts
+++ src/app/components/crear-perfil/crear-perfil.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-crear-perfil',
   templateUrl: './crear-perfil.component.html',
   styleUrls: ['./crear-perfil.component.css']
 })
-export class CrearPerfilComponent {
+
+export class CrearPerfilComponent implements OnInit {
+
+  formCrearPerfil!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.formCrearPerfil = this.formBuilder.group({
+      nombre: [{value: '', disabled: false}, [Validators.required, Validators.maxLength(10)]],
+      edad: [{value:'', disabled:false}, [Validators.required, Validators.maxLength(2)]],
+      pais: [{value: 1, disabled: false}, [Validators.required]],
+      ciudad: [{value: '', disabled: false}, [Validators.required, Validators.maxLength(10)]]
+    })
+  }
+  ngOnInit(): void {
+
+  }
 
 }
