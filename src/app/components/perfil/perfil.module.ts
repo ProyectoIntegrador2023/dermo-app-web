a371f27c81ccd17a8003b73905e838269e34ebb4
MilenaCastilloC <sm.castillo@uniandes.edu.co>
2023-02-26 18:13:47 -0500

    comfiguracion imports exports crear perfil

--- src/app/components/perfil/perfil.module.ts
+++ src/app/components/perfil/perfil.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CrearPerfilComponent } from './crear-perfil/crear-perfil.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CrearPerfilMedicoComponent } from './crear-perfil-medico/crear-perfil-medico.component';
 
 
 @NgModule({
@@ -10,7 +11,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     FormsModule,
     ReactiveFormsModule
   ],
-  declarations: [CrearPerfilComponent],
-  exports: [CrearPerfilComponent]
+  declarations: [CrearPerfilComponent, CrearPerfilMedicoComponent],
+  exports: [CrearPerfilComponent, CrearPerfilMedicoComponent]
 })
 export class PerfilModule { }
