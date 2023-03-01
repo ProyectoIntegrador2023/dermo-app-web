ee51fe2454a0fcd9ec63b7941d858b1b9bb13ca0
MilenaCastilloC <sm.castillo@uniandes.edu.co>
2023-03-01 00:23:24 -0500

    ajuste configuración servicio

--- src/app/components/usuario/usuario.service.ts
+++ src/app/components/usuario/usuario.service.ts
@@ -1,25 +1,37 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { authentication } from 'src/environments/environment';
-import { UserSignUpRq, UserSignUpRs } from './models/userSignUp.model';
-import { UserSignInRq, UserSignInRs } from './models/userSignIn.model';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable, tap} from 'rxjs';
+import {authentication} from 'src/environments/environment';
+import {UserSignUpRq, UserSignUpRs} from './models/userSignUp.model';
+import {UserSignInRq, UserSignInRs} from './models/userSignIn.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
 
   userLogIn(loginDto: UserSignInRq): Observable<UserSignInRs> {
-    const userSignInRq: UserSignInRq = { "email": loginDto.email, "password": loginDto.password };
+    const userSignInRq: UserSignInRq = {"email": loginDto.email, "password": loginDto.password};
     return this.http.post<UserSignInRs>(`${authentication.baseEndpoint}${authentication.loginPath}`, userSignInRq)
+      .pipe(
+        tap((res) => {
+          const token = res.token;
+          sessionStorage.setItem('token', token);
+        })
+      );
   }
 
   userSignUp(signUpDto: UserSignUpRq): Observable<UserSignUpRs> {
-    const userSignUpRq: UserSignUpRq = { "email": signUpDto.email, "password": signUpDto.password };
+    const userSignUpRq: UserSignUpRq = {"email": signUpDto.email, "password": signUpDto.password};
     return this.http.post<UserSignUpRs>(`${authentication.baseEndpoint}${authentication.signupPath}`, userSignUpRq)
+      .pipe(
+        tap((res) => {
+          const token = res.lastLoginAt;
+          sessionStorage.setItem('lastLoginAt', token);
+        })
+      );
   }
-
 }
