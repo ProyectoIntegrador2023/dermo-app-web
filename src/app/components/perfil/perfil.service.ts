ed17b612f2e8c8c8c86e4a5743ec6281688def28
Andres Lopez <am.lopezr1@uniandes.edu.co>
2023-03-02 16:52:36 -0500

    actualizando endpoints

--- src/app/components/perfil/perfil.service.ts
+++ src/app/components/perfil/perfil.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { authentication } from 'src/environments/environment';
+import { AUTH_ENDPOINT } from 'src/environments/environment';
 import { UserProfileRq, UserProfileRs } from './models/userProfile.model';
 import { UserProfileDoctorRq, UserProfileDoctorRs } from './models/userProfileDoctor.model';
 
@@ -13,11 +13,11 @@ export class PerfilService {
 
   userProfile(profileDto: UserProfileRq): Observable<UserProfileRs> {
     const userProfileRq: UserProfileRq = { "nombre": profileDto.nombre, "edad": profileDto.edad, "pais": profileDto.pais, "ciudad": profileDto.ciudad };
-    return this.http.post<UserProfileRs>(`${authentication.baseEndpoint}${authentication.profilePath}`, userProfileRq)
+    return this.http.post<UserProfileRs>(`${AUTH_ENDPOINT.baseEndpoint}${AUTH_ENDPOINT.profilePersonalPath}`, userProfileRq)
   }
 
   userProfileDoctor(profileDoctorDto: UserProfileDoctorRq): Observable<UserProfileDoctorRs> {
     const userProfileDoctorRq: UserProfileDoctorRq = { "especialidad": profileDoctorDto.especialidad, "identificacion": profileDoctorDto.identificacion, "vigencia": profileDoctorDto.vigencia, "licencia": profileDoctorDto.licencia };
-    return this.http.post<UserProfileDoctorRs>(`${authentication.baseEndpoint}${authentication.profilePath}`, userProfileDoctorRq)
+    return this.http.post<UserProfileDoctorRs>(`${AUTH_ENDPOINT.baseEndpoint}${AUTH_ENDPOINT.profileMedicPath}`, userProfileDoctorRq)
   }
 }
--- src/app/components/usuario/usuario.service.ts
+++ src/app/components/usuario/usuario.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable, tap} from 'rxjs';
-import {authentication} from 'src/environments/environment';
+import {AUTH_ENDPOINT} from 'src/environments/environment';
 import {UserSignUpRq, UserSignUpRs} from './models/userSignUp.model';
 import {UserSignInRq, UserSignInRs} from './models/userSignIn.model';
 
@@ -15,7 +15,7 @@ export class UsuarioService {
 
   userLogIn(loginDto: UserSignInRq): Observable<UserSignInRs> {
     const userSignInRq: UserSignInRq = {"email": loginDto.email, "password": loginDto.password};
-    return this.http.post<UserSignInRs>(`${authentication.baseEndpoint}${authentication.loginPath}`, userSignInRq)
+    return this.http.post<UserSignInRs>(`${AUTH_ENDPOINT.baseEndpoint}${AUTH_ENDPOINT.loginPath}`, userSignInRq)
       .pipe(
         tap((res) => {
           const token = res.token;
@@ -26,7 +26,7 @@ export class UsuarioService {
 
   userSignUp(signUpDto: UserSignUpRq): Observable<UserSignUpRs> {
     const userSignUpRq: UserSignUpRq = {"email": signUpDto.email, "password": signUpDto.password};
-    return this.http.post<UserSignUpRs>(`${authentication.baseEndpoint}${authentication.signupPath}`, userSignUpRq)
+    return this.http.post<UserSignUpRs>(`${AUTH_ENDPOINT.baseEndpoint}${AUTH_ENDPOINT.signupPath}`, userSignUpRq)
       .pipe(
         tap((res) => {
           const token = res.lastLoginAt;
