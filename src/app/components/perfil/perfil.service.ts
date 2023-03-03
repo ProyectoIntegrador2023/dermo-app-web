f245a498506c4f1126ef764319b06f4aeda1a8c3
MilenaCastilloC <sm.castillo@uniandes.edu.co>
2023-03-02 23:54:41 -0500

    actualizacion consumo servicio

--- src/app/components/perfil/perfil.service.ts
+++ src/app/components/perfil/perfil.service.ts
@@ -1,23 +1,46 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { AUTH_ENDPOINT } from 'src/environments/environment';
-import { UserProfileRq, UserProfileRs } from './models/userProfile.model';
-import { UserProfileDoctorRq, UserProfileDoctorRs } from './models/userProfileDoctor.model';
+import {HttpClient} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
+import {AUTH_ENDPOINT} from 'src/environments/environment';
+import {UserProfileRq, UserProfileRs} from './models/userProfile.model';
+import {UserProfileDoctorRq, UserProfileDoctorRs} from './models/userProfileDoctor.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PerfilService {
-  constructor(private http: HttpClient){}
-
-  userProfile(profileDto: UserProfileRq): Observable<UserProfileRs> {
-    const userProfileRq: UserProfileRq = { "name": profileDto.name, "age": profileDto.age, "country": profileDto.country, "city": profileDto.city };
-    return this.http.post<UserProfileRs>(`${AUTH_ENDPOINT.baseEndpoint}${AUTH_ENDPOINT.profilePersonalPath}`, userProfileRq)
+  constructor(private http: HttpClient) {
   }
 
-  userProfileDoctor(profileDoctorDto: UserProfileDoctorRq): Observable<UserProfileDoctorRs> {
-    const userProfileDoctorRq: UserProfileDoctorRq = { "specialty": profileDoctorDto.specialty, "licenceId": profileDoctorDto.licenceId, "licenceValidityDate": profileDoctorDto.licenceValidityDate, "licenceImage": profileDoctorDto.licenceImage };
-    return this.http.post<UserProfileDoctorRs>(`${AUTH_ENDPOINT.baseEndpoint}${AUTH_ENDPOINT.profileMedicPath}`, userProfileDoctorRq)
+  userProfile(profileDto: UserProfileRq): Observable<any> {
+    const userProfileRq: UserProfileRq = {
+      name: profileDto.name,
+      age: profileDto.age,
+      country: profileDto.country,
+      city: profileDto.city,
+      email: sessionStorage.getItem('email') as string
+    };
+    return this.http.post<any>(`${AUTH_ENDPOINT.baseEndpoint}${AUTH_ENDPOINT.profilePersonalPath}`,
+      userProfileRq,
+      {
+        observe: 'response'
+      }
+    )
+  }
+
+  userProfileDoctor(profileDoctorDto: UserProfileDoctorRq): Observable<any> {
+    const userProfileDoctorRq: UserProfileDoctorRq = {
+      specialty: profileDoctorDto.specialty,
+      licenceId: profileDoctorDto.licenceId,
+      licenceValidityDate: profileDoctorDto.licenceValidityDate,
+      licenceImage: profileDoctorDto.licenceImage,
+      email: sessionStorage.getItem('email') as string
+    };
+    return this.http.post<any>(`${AUTH_ENDPOINT.baseEndpoint}${AUTH_ENDPOINT.profileMedicPath}`,
+      userProfileDoctorRq,
+      {
+        observe: 'response'
+      }
+    )
   }
 }
