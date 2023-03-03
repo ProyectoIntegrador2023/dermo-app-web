d6c15a7b13e582caffd1890e6ac550a4be4ebbf4
MilenaCastilloC <sm.castillo@uniandes.edu.co>
2023-03-02 23:57:34 -0500

    configuracion carga licencia

--- src/app/components/perfil/crear-perfil-medico/crear-perfil-medico.component.ts
+++ src/app/components/perfil/crear-perfil-medico/crear-perfil-medico.component.ts
@@ -1,21 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-import { JwtHelperService } from '@auth0/angular-jwt';
-import { ToastrService } from 'ngx-toastr';
-import { LoaderService } from 'src/app/services/loader.service';
-import { UserProfileDoctorRq } from '../models/userProfileDoctor.model';
-import { PerfilService } from '../perfil.service';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
+import {JwtHelperService} from '@auth0/angular-jwt';
+import {ToastrService} from 'ngx-toastr';
+import {LoaderService} from 'src/app/services/loader.service';
+import {UserProfileDoctorRq} from '../models/userProfileDoctor.model';
+import {PerfilService} from '../perfil.service';
+import {finalize} from "rxjs";
 
 @Component({
   selector: 'app-crear-perfil-medico',
   templateUrl: './crear-perfil-medico.component.html',
   styleUrls: ['./crear-perfil-medico.component.css']
 })
-export class CrearPerfilMedicoComponent implements OnInit{
+export class CrearPerfilMedicoComponent implements OnInit {
 
   helper = new JwtHelperService();
   profileDoctorDto: UserProfileDoctorRq;
+  imageBase64: any;
+  imagePreview: any;
 
   formCrearPerfilMedico!: FormGroup;
 
@@ -24,69 +27,82 @@ export class CrearPerfilMedicoComponent implements OnInit{
     private toastr: ToastrService,
     private loaderService: LoaderService,
     private perfilService: PerfilService,
-    private router: Router) { }
+    private router: Router) {
+  }
 
   ngOnInit(): void {
     this.formCrearPerfilMedico = this.formBuilder.group({
-      especialidad: [{ value: 1, disabled: false }, [Validators.required]],
-      identificacion: [{ value: '', disabled: false }, [Validators.required, Validators.maxLength(10)]],
-      vigencia: [{ value: '', disabled: false }, [Validators.required, Validators.maxLength(2)]],
-      licencia: [null]
+      specialty: [{value: 1, disabled: false}, [Validators.required]],
+      licenceId: [{value: '', disabled: false}, [Validators.required, Validators.maxLength(10)]],
+      licenceValidityDate: [{value: '', disabled: false}, [Validators.required]]
     });
   }
 
   error = false;
 
   onImageSelected(event: Event) {
+    this.loaderService.show();
     const inputElement = event.target as HTMLInputElement;
     if (inputElement && inputElement.files && inputElement.files.length) {
       const file = inputElement.files[0];
-      this.formCrearPerfilMedico.patchValue({
-        licencia: file
-      });
+      this.getBase64(file, (base64: any) => {
+        this.imageBase64 = base64.split(',').pop();
+        this.imagePreview = base64;
+      })
+    }
+  }
+
+  getBase64(file: Blob, callback: (base64: any) => void) {
+    let fileReader = new FileReader();
+    fileReader.readAsDataURL(file);
+    fileReader.onload = () => {
+      callback(fileReader.result);
+      this.loaderService.hide();
+    };
+    fileReader.onerror = (error) => {
+      console.error(error);
+      this.loaderService.hide();
     }
   }
 
   onCreatePerfilDoctor() {
+    console.log(this.formCrearPerfilMedico)
     if (this.formCrearPerfilMedico.invalid) {
       return;
     }
     this.error = false
     this.profileDoctorDto = this.formCrearPerfilMedico.value;
+    this.profileDoctorDto.licenceImage = this.imageBase64;
     this.loaderService.show();
-    this.router.navigate(['/home-in/crear-perfil-medico'])
-    this.perfilService.userProfileDoctor(this.profileDoctorDto)
-      .subscribe(res => {
-        console.log(res)
-        const token = res.token;
-        sessionStorage.setItem('especialidad', token);
-        this.showSuccess()
-        this.router.navigate([`/signup`])
+    this.perfilService.userProfileDoctor(this.profileDoctorDto).pipe(
+      finalize(() => {
+        this.loaderService.hide();
+      })
+    ).subscribe({
+      next: (res: any) => {
+        console.log(res);
+        if (res.body.statusCode >= 400) {
+          this.error = true;
+          this.showError(res.message);
+        } else {
+          this.showSuccess();
+          this.router.navigate(['/home-in']);
+        }
       },
-        error => {
-          console.error(error);
-          this.error = true
-        })
+      error: (error) => {
+        console.error(error);
+        this.error = true;
+        this.showError(error.error.message);
+      }
+    })
   }
-
-  // guardarEspecialidad() {
-  //   console.log(this.formCrearPerfilMedico);
-  //   console.log(this.formCrearPerfilMedico.value);
-  //   console.log(this.formCrearPerfilMedico.value.especialidad);
-  //   console.log(this.formCrearPerfilMedico.value.identificacion);
-  //   console.log(this.formCrearPerfilMedico.value.vigencia);
-  //   console.log(this.formCrearPerfilMedico.value.llicencia);
-  //   this.loaderService.show();
-  //   setTimeout(
-  //     ()=>{
-  //         this.loaderService.hide();
-  //         this.toastr.success("Tu perfil médico ha sido creado", "Creación especialidad");
-  //     }, 3000);
-  // }
 
   showSuccess() {
     this.toastr.success(`Tu perfil ha sido creado.`, "Creación Perfil");
-    this.loaderService.hide();
+  }
+
+  showError(error: string) {
+    this.toastr.error(error, "Error")
   }
 
   get formPerfilMedico() {
