9d5aa123994572b891465b0665edbe54caf68d07
MilenaCastilloC <sm.castillo@uniandes.edu.co>
2023-03-04 21:36:44 -0500

    configuracion casos component

---
+++ src/app/components/diagnostico/casos/casos.component.ts
@@ -0,0 +1,65 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-casos',
+  templateUrl: './casos.component.html',
+  styleUrls: ['./casos.component.css']
+})
+export class CasosComponent {
+  casos:any[]=[
+    {
+      id: 1,
+      caso: 'Caso 1',
+      detalle: 'Ver detalle',
+      imagen: 'assets/img/lesion.svg'
+    },
+    {
+      id: 1,
+      caso: 'Caso 1',
+      detalle: 'Ver detalle',
+      imagen: 'assets/img/lesion.svg'
+    },
+    {
+      id: 1,
+      caso: 'Caso 1',
+      detalle: 'Ver detalle',
+      imagen: 'assets/img/lesion.svg'
+    },
+    {
+      id: 1,
+      caso: 'Caso 1',
+      detalle: 'Ver detalle',
+      imagen: 'assets/img/lesion.svg'
+    },
+    {
+      id: 1,
+      caso: 'Caso 1',
+      detalle: 'Ver detalle',
+      imagen: 'assets/img/lesion.svg'
+    },
+    {
+      id: 1,
+      caso: 'Caso 1',
+      detalle: 'Ver detalle',
+      imagen: 'assets/img/lesion.svg'
+    },
+    {
+      id: 1,
+      caso: 'Caso 1',
+      detalle: 'Ver detalle',
+      imagen: 'assets/img/lesion.svg'
+    },
+    {
+      id: 1,
+      caso: 'Caso 1',
+      detalle: 'Ver detalle',
+      imagen: 'assets/img/lesion.svg'
+    },
+    {
+      id: 1,
+      caso: 'Caso 1',
+      detalle: 'Ver detalle',
+      imagen: 'assets/img/lesion.svg'
+    }
+  ];
+}
