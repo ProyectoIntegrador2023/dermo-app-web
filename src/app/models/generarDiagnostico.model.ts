4ea91f684d83a7db244765836a712d3ca41e80f2
MilenaCastilloC <sm.castillo@uniandes.edu.co>
2023-03-04 21:40:21 -0500

    configuracion generar-diagnostico

--- src/app/models/generarDiagnostico.model.ts
+++ src/app/models/generarDiagnostico.model.ts
@@ -1,14 +1,17 @@
 export interface GenerarDiagnosticoRq {
-  condition: number;
+}
+
+export interface GenerarDiagnosticoRs {
+  id: number;
+  medicId: number;
+  injuryId: number;
+  condition: string;
   level: string;
   requeresTreatment: true;
   treatmentTerm: string;
   medicines: string;
   treatmentControl: string;
   recommendations: string;
-  medicId: string
-}
-
-export interface GenerarDiagnosticoRs {
-
+  createdAt: string;
+  updatedAt: string;
 }
--- src/app/services/generar-diagnostico.service.ts
+++ src/app/services/generar-diagnostico.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Injectable({
   providedIn: 'root'
 })
 export class GenerarDiagnosticoService {
+  constructor(
+    private readonly router: Router,
+  ) { }
 
-  constructor() { }
 }
