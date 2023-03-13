579ab68758c99f210a2674b3f0948aa34b5a1bd6
Andres Lopez <am.lopezr1@uniandes.edu.co>
2023-03-13 02:31:58 -0500

    fix bugs & coverage report

--- src/app/components/home-in/home-in.component.ts
+++ src/app/components/home-in/home-in.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { finalize } from 'rxjs';
+import { LoaderService } from 'src/app/services/loader.service';
+import { PerfilService } from '../perfil/perfil.service';
+import { UsuarioService } from '../usuario/usuario.service';
 
 
 @Component({
@@ -6,5 +10,34 @@ import { Component } from '@angular/core';
   templateUrl: './home-in.component.html',
   styleUrls: ['./home-in.component.css']
 })
-export class HomeInComponent {
+export class HomeInComponent implements OnInit {
+
+  constructor(
+    private readonly usuarioService: UsuarioService,
+    private readonly perfilService: PerfilService,
+    private readonly loaderService: LoaderService,
+  ) {}
+
+  ngOnInit(): void {
+    const medicId = sessionStorage.getItem('medicId');
+    if(!medicId) {
+      this.getMedicId();
+    }
+  }
+
+  getMedicId() {
+    this.perfilService.getUserProfile().pipe(
+      finalize(() => {
+        this.loaderService.hide();
+      })
+    ).subscribe({
+      next: (res: any) => {
+        console.log(res);
+        sessionStorage.setItem('medicId', res.body.id);
+      },
+      error: (error) => {
+        console.error(error);
+      }
+    })
+  }
 }
